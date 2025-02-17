55be1c2d269ac1ec43feeef00556ac2cf3bff63e
Peter Stangl <peter.stangl@ph.tum.de>
2025-02-17 22:46:47 +0100

    catch BlockingIOError during cache update

--- src/rgevolve/tools/utils.py
+++ src/rgevolve/tools/utils.py
@@ -24,22 +24,33 @@ def update_cache(cache_path, package_name, data_h5):
         mode = "r+"  # Open in read/write mode if it exists
     else:
         mode = "w"   # Create a new file if it doesn't exist
-    with h5py.File(cache_path, mode) as h5file:
-        if mode == "r+":
-            for sector, RG_evolution in data_h5['RG evolution'].items():
-                if sector in h5file and h5file[sector].attrs['hash'] == hash(RG_evolution):
-                    continue
-                elif sector in h5file:
-                    print(f"Updating cache file for {package_name}")
-                    print(f"Updating sector {sector} (hash mismatch)")
-                    inverses = np.array([
-                        np.linalg.inv(matrix)
-                        for matrix in RG_evolution
-                    ])
-                    h5file[sector][...] = inverses
-                    h5file[sector].attrs['hash'] = hash(RG_evolution)
-                else:
-                    print(f"Updating cache file for {package_name}")
+    try:
+        with h5py.File(cache_path, mode) as h5file:
+            if mode == "r+":
+                for sector, RG_evolution in data_h5['RG evolution'].items():
+                    if sector in h5file and h5file[sector].attrs['hash'] == hash(RG_evolution):
+                        continue
+                    elif sector in h5file:
+                        print(f"Updating cache file for {package_name}")
+                        print(f"Updating sector {sector} (hash mismatch)")
+                        inverses = np.array([
+                            np.linalg.inv(matrix)
+                            for matrix in RG_evolution
+                        ])
+                        h5file[sector][...] = inverses
+                        h5file[sector].attrs['hash'] = hash(RG_evolution)
+                    else:
+                        print(f"Updating cache file for {package_name}")
+                        print(f"Adding sector {sector}")
+                        inverses = np.array([
+                            np.linalg.inv(matrix)
+                            for matrix in RG_evolution
+                        ])
+                        h5file.create_dataset(sector, data=inverses, compression="gzip")
+                        h5file[sector].attrs['hash'] = hash(RG_evolution)
+            else:
+                print(f"Creating new cache file for {package_name}")
+                for sector, RG_evolution in data_h5['RG evolution'].items():
                     print(f"Adding sector {sector}")
                     inverses = np.array([
                         np.linalg.inv(matrix)
@@ -47,16 +58,8 @@ def update_cache(cache_path, package_name, data_h5):
                     ])
                     h5file.create_dataset(sector, data=inverses, compression="gzip")
                     h5file[sector].attrs['hash'] = hash(RG_evolution)
-        else:
-            print(f"Creating new cache file for {package_name}")
-            for sector, RG_evolution in data_h5['RG evolution'].items():
-                print(f"Adding sector {sector}")
-                inverses = np.array([
-                    np.linalg.inv(matrix)
-                    for matrix in RG_evolution
-                ])
-                h5file.create_dataset(sector, data=inverses, compression="gzip")
-                h5file[sector].attrs['hash'] = hash(RG_evolution)
+    except BlockingIOError:
+        pass
 
 def load_data(package_name):
     data_path = get_data_path(package_name)
