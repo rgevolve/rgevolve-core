c8107018f75a6f349d6854cfc6d5eba3dc0aa15c
Aleks Smolkovic <67457577+alekssmolkovic@users.noreply.github.com>
2025-05-21 14:25:36 +0200

    add reference scale to init (#4)

--- src/rgevolve/tools/__init__.py
+++ src/rgevolve/tools/__init__.py
@@ -3,5 +3,6 @@ from . import utils
 from .functions import (
     get_wc_basis, compute_observable_RGs, wc_real_to_real_sector_idx, mu_wet as m_Z,
     run_and_match, get_wc_mask, get_scales, get_sector_indices, matching_sectors,
-    efts_available, bases_available, bases_installed
+    efts_available, bases_available, bases_installed,
+    reference_scale,
 )
